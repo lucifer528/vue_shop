a1db0e493fc9a2fe2891ed08d3cf61e079fe9c11
lucifer528 <276550491@qq.com>
2020-07-17 11:02:39 +0800

    完成了登陆

---
+++ src/components/Login.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="">
+      </div>
+      <el-form ref="loginFormRef" :rules="loginFormRules"
+      :model="loginForm" label-width="0px" class="login_form">
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input prefix-icon="iconfont icon-user"
+          v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input prefix-icon="iconfont icon-3702mima"
+          show-password v-model="loginForm.password"></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登陆</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入登陆名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入登陆密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    resetLoginForm () {
+      // 重置表单,这个方法是 elment-ui 得供的
+      this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate(async (valid, tip) => {
+        // console.log(valid)
+        // console.log(tip)
+        if (!valid) return
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        console.log(res)
+        if (res.meta.status !== 200) {
+          // console.log(res.meta.msg)
+          this.$message.error('登陆失败')
+        } else {
+          this.$message.success('登陆成功')
+          // 1. 将登陆成功之后的 token, 保存到客户端的 sessionStorage 中
+          //   1.1 项目中除了登陆之外的其他API接口,必须在登录之后才能访问
+          //   1.2 token 只应在当前网站打开期间生效,所以将 token 保存在 sessionStorage
+          // 2. 通过编程式导航跳转到后台主页,路由地址是 /home
+          window.sessionStorage.setItem('token', res.data.token)
+          this.$router.push('/home')
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+  .btns {
+    display: flex;
+    justify-content: flex-end;
+  }
+  .login_form {
+    position: absolute;
+    bottom: 0 ;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+  }
+}
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,12 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+// import { Button } from 'element-ui'
+// import { Form, FormItem } from 'element-ui'
+// import { Input } from 'element-ui'
+// eslint 检查下上面报错,不要重复从一个里引入...
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
 Vue.use(Button)
+Vue.prototype.$message = Message
